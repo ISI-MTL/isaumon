9d748d332b4571025d286604bf6397079f502721
unknown <kparent@L-P-01.isimtl.local>
2013-09-18 12:39:19 -0400

    Ajout des doctypes dans chancun des documents HTML

---
+++ html/contact_poisson.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Contact : Partagez vos connaissances sur le saumon</title>
+		<meta charset="utf-8">
+		<meta name="keywords" content="education, connaissances, partage, saumon, omega-3, informations, idees, recettes, culture, commentaires, commentaire, remarques, cholesterol, suggestions, salmonide ">
+		<meta name="description" content="Questions, suggestions, écrivez-nous et partagez votre avis sur le saumon, ses bienfaits. Faites découvrir vos recettes et expériences cullinaires">
+	</head>
+	<body>
+	
+	</body>
+</html>
