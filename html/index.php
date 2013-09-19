33720eac4c27d9cd35367d746d9a1ffc510ee2d9
Mathieu Collinge <shabla@gmail.com>
2013-09-19 10:16:14 -0400

    structure de base pour le template
    - Modification de index.php pour inclure tous les fichiers nécessaire à
    chaque page.
    - Tous les liens doivent pointer vers
    "index.php?page=nomDuFichierPHPdeLaPage" et les fichiers nécessaires
    (css, js, config, doivent être nommés de la même façon)

--- html/index.php
+++ html/index.php
@@ -1,12 +1,79 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>iSaumon.com: Bienfaits et bénéfices du saumon</title>
 		<meta charset="utf-8">
-		<meta name="keywords" content="informations, recette, cuisine, saumon, poisson, benefices, peche, cholesterol, omega3, immunodepresseur, produits naturels, elevage, informations, repas">
-		<meta name="description" content="Site informationnel sur les avantages et bénéfices de la consommation. Visitez nos pages de recettes ou informez-vous sur les avantages nutrionnels du poisson.">
+		<?php
+			/*
+			 *	Le titre, les keywords et la description de la page se trouve dans le fichier config 
+			 *	associé avec la page. Par exemple quand la variable GET page est égale à "contact", le fichier 
+			 *	"config/contact.config.php.inc" est inclus et celui-ci contient les variables nécessaires.
+			 */
+			if( isset($_GET['page']) )
+			{
+				if( file_exists("config/" . $_GET['page'] . ".config.php.inc") )
+				include("config/" . $_GET['page'] . ".config.php.inc");
+			}
+			else
+			{
+				include("config/accueil.config.php.inc");
+			}
+		?>
+
+		<title><?php if( isset($titre) ) echo $titre; else echo "iSaumon.com"; ?></title>
+		<meta name="keywords" content="<?php if(isset($keywords)) echo $keywords; ?>">
+		<meta name="description" content="<?php if(isset($description)) echo $description; ?>">
+
+		<!-- jQuery 1.10..2 -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+		<!-- Style de base -->
+		<link rel="stylesheet" type="text/css" href="../assets/css/base.css" />
+
+		<!-- Script de base -->
+		<script type="text/javascript" src="../assets/js/base.js">Vous devez activer JavaScript</script>
+
+		<!-- CSS et JS spécifique à la page -->
+		<?php
+		if( isset($_GET['page']) )
+		{
+			if( file_exists("../css/" . $_GET['page'] . ".css") )
+				echo('<link rel="stylesheet" href="../css/' . $_GET["page"] . '.css">');
+			if( file_exists("../js/" . $_GET['page'] . ".js") )
+				echo('<script src="../js/' . $_GET["page"] . '.js"></script>');
+		}
+		else
+		{
+			if( file_exists("css/home.css") )
+				echo('<link rel="stylesheet" type="text/css" href="css/home.css">');
+			if( file_exists("js/home.js") )
+				echo('<script src="js/home.js"></script>');
+		}
+		?>
 	</head>
 	<body>
-	
+		<?php include("includes/header.php.inc"); ?>
+
+		<div id="contenu">
+			<?php
+			if( isset($_GET['page']) )
+			{
+				$filename = $_GET['page'] . ".php";
+				if( file_exists($filename) )
+					include($filename);
+				else
+				{
+					// le fichier n'existe pas
+				}
+			}
+			else
+			{
+				// la variagle get "page" n'est pas initialisée
+				// redirige vers la page d'accueil
+				include("accueil.php");
+			}
+			?>
+		</div>
+
+		<?php include("includes/footer.php.inc") ?>
 	</body>
 </html>
