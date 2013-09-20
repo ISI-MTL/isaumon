4be812f034353ff3a3309db4d089289f81ec087d
Mathieu Collinge <shabla@gmail.com>
2013-09-20 11:36:00 -0400

    intégration du blog

--- blog.php
+++ blog.php
@@ -3,9 +3,9 @@
 	<article>
 		<img src="assets/img/bouchees-saumon-roti_a_la_francaise.jpg" alt="" />
 		<div>
-			<h2>La revanche des saumons</h2>
-			<p>Par Ricardo Seedo | 19 septembre 2013</p>
-			<p>
+			<h2>Les saumons contre-attaque</h2>
+			<p class="chroniqueur">Par Ricardo Seedo | 19 septembre 2013</p>
+			<p class="chronique-contenu">
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -13,8 +13,34 @@
 				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 			</p>
-			<a href="#">LIRE LA SUITE</a>
+			<a href="#" class="lire-tout">LIRE LA SUITE</a>
 		</div>
 	</article>
 
+	<article>
+		<img src="assets/img/bouchees-saumon-roti_a_la_francaise.jpg" alt="" />
+		<div>
+			<h2>Les saumons contre-attaque</h2>
+			<p class="chroniqueur">Par Ricardo Seedo | 19 septembre 2013</p>
+			<p class="chronique-contenu">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<a href="#" class="lire-tout">LIRE LA SUITE</a>
+		</div>
+	</article>
+
+	<nav>
+		<ul>
+			<li><a href="#">1</a></li>
+			<li><a href="#">2</a></li>
+			<li><a href="#">3</a></li>
+			<li><a href="#">4</a></li>
+		</ul>
+	</nav>
+	
 </div>
