a1f4ec81115e1f245f44145e228d33062ee4e0a8
Mathieu Collinge <shabla@gmail.com>
2013-09-20 10:50:40 -0400

    nouvelles pages, modif menu, accueil, blog

--- apropos.php
+++ apropos.php
@@ -1,3 +1,3 @@
 <div class="container">
-	formulaire de contact
+	apropos
 </div>
---
+++ blog.php
@@ -0,0 +1,20 @@
+<div class="container">
+	<h1>Nos chroniques</h1>
+	<article>
+		<img src="assets/img/bouchees-saumon-roti_a_la_francaise.jpg" alt="" />
+		<div>
+			<h2>La revanche des saumons</h2>
+			<p>Par Ricardo Seedo | 19 septembre 2013</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<a href="#">LIRE LA SUITE</a>
+		</div>
+	</article>
+
+</div>
