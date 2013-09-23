e48b33fe965d2b56cf684a1cb8db05774513d091
Mathieu Collinge <shabla@gmail.com>
2013-09-23 10:45:18 -0400

    php de la page de blog

--- blog.php
+++ blog.php
@@ -4,56 +4,60 @@
 	<h1>Nos chroniques</h1>
 
 <?php
-// Affiche $postsDisplayed posts en fonction de la $page courrante
-foreach($articles as $post)
+$page = 1;
+
+if( isset($_GET['p']) )
+	$page = $_GET['p'];
+
+// articlesParPage se trouve dans le fichier de config config/blog.config.php.inc
+$articles = getArticlesFromPage($page, $articlesParPage);
+
+// Affiche $articlesParPage posts en fonction de la $page courrante
+foreach($articles as $article)
 { ?>
 
 	<article class="chronique">
-		<img src="assets/img/bouchees-saumon-roti_a_la_francaise.jpg" alt="" />
+		<?php
+		// si l'image n'existe pas, afficher une image par défaut
+		$image = "missing.png";
+		if( isset($article['img']) && file_exists("assets/img/blog/" . $article['img']) )
+			$image = $article['img'];
+		?>
+		<img src="assets/img/blog/<?php echo $image; ?>" alt="" />
 		<div>
-			<h2>Les saumons contre-attaque</h2>
-			<p class="chroniqueur">Par Ricardo Seedo | 19 septembre 2013</p>
+			<h2><?php echo $article['titre']; ?></h2>
+			<p class="chroniqueur">Par <?php echo $article['auteur']; ?> à <?php echo date_format( date_create($article['date']), 'G:i, d-m-Y'); ?></p>
 			<p class="chronique-contenu">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				<?php echo $article['contenu']; ?>
 			</p>
-			<a href="#" class="lire-tout">LIRE LA SUITE</a>
+			<a href="?page=blog&id=<?php echo $article['id'] ?>" class="lire-tout">LIRE LA SUITE</a>
 		</div>
 	</article>
 
 <?php
 }
 
+// si c'est la liste des articles
 if( !isset($_GET['id']) )
 {
-	// détermine le nombre de pages pour la pagination
-	$nbPages = 1;
-	$nbPosts = getNumberOfPosts();
-	if( $nbPosts != -1 )
-	{
-		$nbPages = ceil( $nbPosts / $postsDisplayed );
-	}
-
 	// Pagination 
-	echo '<ul id="pagination">';
+	echo '<nav>';
+	echo '<ul>';
 	for($i=1; $i<=$nbPages; $i++)
 	{
 		// si t est absent et que $i = 1, alors la première page est active
 		// si t est présent, alors le $i ème élément est actif
-		if( (!isset($_GET['t']) && $i == 1) || (isset($_GET['t']) && $i == $_GET['t']) )
+		if( (!isset($_GET['p']) && $i == 1) || (isset($_GET['p']) && $i == $_GET['p']) )
 			echo '<li class="active">';
 		else
 			echo '<li>';
-		echo '<a href="?t='.$i.'">'.$i.'</a>';
+		echo '<a href="?page=blog&p='.$i.'">'.$i.'</a>';
 		echo '</li>';
 	}
 	echo '</ul>';
+	echo '</nav>';
 }
-else
+else // si c'est un article en particulier on affiche la section commentaires
 {
 	// Si $_SESSION['user'] est setté, afficher le formulaire pour ajotuer un comment
 	/*if( isset($_SESSION['user']) )
@@ -65,16 +69,6 @@ else
 	//$comments = 
 }
 
-?>
-
-
-	<nav>
-		<ul>
-			<li><a href="#">1</a></li>
-			<li><a href="#">2</a></li>
-			<li><a href="#">3</a></li>
-			<li><a href="#">4</a></li>
-		</ul>
-	</nav>
-	
+?>	
 </div>
+
