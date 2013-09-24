ee6d55ca21ee52060073d7c7faf91e445019aee8
Jacques Robidoux <4ever22.jr@gmail.com>
2013-09-24 10:16:51 -0500

    page erreur avec css et une photo

---
+++ includes/erreur_404_iSaumon.php
@@ -0,0 +1,5 @@
+<div id="erreur">
+	<img src="../assets/img/erreur_404_saumon.png" alt="erreur_404_iSaumon">
+	<p>La page que vous cherchez n'existe pas ou à été déplacée</p>
+	<a href="index.html">Revenir sur la Home page</a>
+</div>
