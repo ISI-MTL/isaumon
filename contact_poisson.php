d3830f07fa6fcf83b835e3fea5eb1f0904a381c7
gmarcopolo <g.marcopolo@gmail.com>
2013-09-24 09:24:05 -0400

    Début contenu contact_poisson
    Introduction des sections pour inclure le prenom, nom, courriel et
    commentaire

--- contact_poisson.php
+++ contact_poisson.php
@@ -1,3 +1,26 @@
 <div class="container">
-	formulaire de contact
+	<h1>Gardez Contact</h1>
+
+	<h2>Réagissez à notre blog ou faits nous parvenir vos commentaires, votre avis est important</h2>
+
+	<h3>Vos coordonés</h3>
+		
+		<label>Prénom
+			<input  type="text" placeholder="Prénom"  name="prenom" pattern="^[a-zA-Z][a-zA-Z-_\.\s]{1,20}$" title="Veuillez introduire votre prénom s'il vous plaît" required="required" value="p.ex. Jean-François"/>
+    	</label>
+        
+        <label>Nom de famille
+    		<input type="text" placeholder="Nom de famille" name="nom" required pattern="^[a-zA-Z\][a-zA-Z0-9-_\.\s]{1,20}$" title="Veuillez introduire votre nom de famille s'il vous plaît" value="p.e. Doubois"/>
+    	</label>
+        
+        <label>Email / courriel
+        	<input type="email" placeholder="Courriel" name="email" title="Veuillez introduire votre email/courriel s'il vous plaît" value="hola"/>
+        </label>
+
+	        
+
+	<h3>Commentaires</h3>
+	<textarea name= "ecrireCommentaires" id="commentaires"></textarea>
+
+
 </div>
