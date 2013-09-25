08647d067a3b6a1238267d1bad80acad71f9be9f
Ricardo Seedoo <sid25ric@gmail.com>
2013-09-25 10:42:18 -0400

    modif

--- recettes_saumon.php
+++ recettes_saumon.php
@@ -1,18 +1,19 @@
-<?php
-require_once("recettes/recettes_manager.php.inc");
-$table_recettes = get_all_recette();
-?>
+	<?php
+	require_once("/recettes/recettes_manager.php.inc");
+	$table_recettes=get_all_recette();
+	//var_dump($table_recettes);
+	//	var_dump($table_recettes[0]["titre"]);	
+	?>
+
 
 <div class="container">
 	<h1>Nos recettes rapide et facile!</h1>
 	<h2><cite>"Cuisiner suppose une tête légère, un esprit généreux et un coeur large".</cite>par Paul Gauguin</h2>
 	<ul class="grid effect-7" id="grid">
-		<?php foreach( $table_recettes as $recette){ ?>
-			<li>
-				<img class="recette_image" src="assets/img/recettes/<?php echo($recette["img"]);?>">
+				<?php foreach( $table_recettes as $recette){ ?>
+				<li><img class="recette_image" src="assets/img/recettes/<?php echo($recette["img"]);?>">
 				<h2><?php echo($recette["titre"]);?></h2>
 				<p><?php echo($recette["commentaire"]);?></p>
-<<<<<<< HEAD
 				<a href="">En savoir plus</a>
 				</li>
 				<?php } ?>
@@ -26,10 +27,6 @@ $table_recettes = get_all_recette();
 				<li><a href="http://drbl.in/fWMM"><img src="assets/img/recettes/1.jpg"></a></li>
 				<li><a href="http://drbl.in/fWPV"><img src="assets/img/recettes/3.jpg"></a></li>
 				<li><a href="http://drbl.in/fWMT"><img src="assets/img/recettes/4.jpg"></a></li>
-=======
-				<a href="?page=recette&id=<?php echo($recette['id']); ?>">En savoir plus</a>
-			</li>
-		<?php } ?>
->>>>>>> 8ed49dd7ed7c9f737ee2c294b04788b18ba9b667
 	</ul>
 </div>
+
