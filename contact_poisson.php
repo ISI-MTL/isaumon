522d1a6b3b8a5d79df8121ae02286023491e4581
alune <diopeur@gmail.com>
2013-09-26 11:08:49 -0400

    page accueil modif

--- contact_poisson.php
+++ contact_poisson.php
@@ -1,56 +1,51 @@
+<?php 
+	
+?>
 
+
+
+
+
+<!-- ================================================================= -->
 <div class="container">
 	<h1>Gardez Contact</h1>
+	<img id="timbres" src="assets\img\timbres\timbres-poisson.png">
+	<h2>Réagissez à notre blog ou faites-nous parvenir vos commentaires,<br>votre avis est important</h2>
 
-	<h2>Réagissez à notre blog ou faites-nous parvenir vos commentaires, votre avis est important</h2>
-
-	
-	<div class= "ensemble">
+	<form class= "ensemble" action = "traitement_contact.php.inc" method = "POST">
 		<div class= "champsText">
 			<h3>Vos coordonés</h3>
-			<label >Prénom : </label>
-				<input  type="text" placeholder="p.ex. Jean-François"  name="prenom" pattern="^[a-zA-Z][a-zA-Z-_\.\s]{1,20}$" title="Veuillez introduire votre prénom s'il vous plaît" required="required" value=""/>
-	    	
+			<label> Prénom : </label>
+				<input  type="text" placeholder="p.ex. Jean-François"  name="prenom" pattern="^[a-zA-Z][a-zA-Z-_\.\s]{1,20}$" title="Veuillez introduire votre prénom s'il vous plaît" required="required" value=""/> 	
 	    	<br>
 	    	<br>
 	    	<br>
 	        
-	        <label>Nom de famille : </label>
+	        <label> Nom de famille : </label>
 	    		<input type="text" placeholder="p.ex. Doubois" name="nom" pattern="^[a-zA-Z\][a-zA-Z0-9-_\.\s]{1,20}$" title="Veuillez introduire votre nom de famille s'il vous plaît" value=""/>
-	    	
 	    	<br>
 	    	<br>
 	    	<br>
-	        
-	        <label>Email / courriel : </label>
+	        <label> Email / courriel : </label>
 	        	<input type="email" placeholder="p.ex. jf-doubois@courriel.com" name="email" title="Veuillez introduire votre email/courriel s'il vous plaît" value=""/>
-	        
-	    </div>
-	    
-	    
-	      
+	    </div>  
 	    <div class= "champsText">
 		    <h3>Votre avis : </h3>
-			<label>Commentaires : </label>
+			<label> Commentaires : </label>
 				<br>
 				<textarea name= "ecrireCommentaires" id="commentaires"></textarea>
-			
 			<br>
 			<br>
-			
-
-			<label>Aimez-vous ce site?</label>
+			<label> Aimez-vous iSaumon? </label>
 				<fieldset > 
-				    <input type="radio" id="star5" name="rating[]" value="5" />
-				    <label for="star5" title="Extraordinaire!">5 stars</label>
-				    
-				    <input type="radio" id="star4" name="rating[]" value="4" /><label for="star4" title="J'aime">4 stars</label>
-				    <input type="radio" id="star3" name="rating[]" value="3" /><label for="star3" title="OK">3 stars</label>
-				    <input type="radio" id="star2" name="rating[]" value="2" /><label for="star2" title="Je n'aime pas">2 stars</label>
-				    <input type="radio" id="star1" name="rating[]" value="1" /><label for="star1" title="Je n'aime pas du tout">1 star</label>
+				    <input type="radio" id="star5" name="rating" value="Extraordinaire!" /><label for="star5" title="Extraordinaire!"></label>
+				    <input type="radio" id="star4" name="rating" value="J'aime" /><label for="star4" title="J'aime"></label>
+				    <input type="radio" id="star3" name="rating" value="OK" /><label for="star3" title="OK"></label>
+				    <input type="radio" id="star2" name="rating" value="Je n'aime pas" /><label for="star2" title="Je n'aime pas"></label>
+				    <input type="radio" id="star1" name="rating" value="Je n'aime pas du tout" /><label for="star1" title="Je n'aime pas du tout"></label>
 				</fieldset>
 		</div>
-	</div>
-
-
+		<button>Envoyer</button>
+	</form>
+	
 </div>
