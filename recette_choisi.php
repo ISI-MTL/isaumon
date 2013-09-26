21eadf18412aaab97e05d7b340ed47dda94d86e8
Ricardo Seedoo <sid25ric@gmail.com>
2013-09-26 11:31:00 -0400

    recette choisi
    recette choisi

--- recette_choisi.php
+++ recette_choisi.php
@@ -3,16 +3,16 @@
 	if (isset($_GET['recette'])) {
 		$recette=get_recette_by_id($_GET['recette']);
 	}
-//	var_dump($recette);
 ?>
 <div class="container">
 <h2><?php echo($recette[0]['titre']); ?></h2>
-<img src="<?php echo($recette[0]['img']); ?>">
+<img src="assets/img/recettes/<?php echo($recette[0]["img"])?>">
 
 <!-- la section Ingredient -->
-<section>
+<section id="ingredient">
+<h3>Ingredients</h3>
 <?php 
-$ingredients =explode(";", ($recette[0]['ingredients']));
+$ingredients = explode(";", ($recette[0]['ingredients']));
 foreach($ingredients as $value){
 echo ($value."<br/>");
 }
@@ -20,7 +20,16 @@ echo ($value."<br/>");
 </section>
 
 <!-- la section preparation -->
-<section>
-<?php echo($recette[0]['preparation']); ?>	
+<section id ="preparation">
+<h3>Preparation</h3>
+<?php 
+$preparation = explode(".", ($recette[0]['preparation']));
+$prep_len = count($preparation);
+
+for($i=0 ; $i < ($prep_len-1); $i++){
+
+echo ("- ".$preparation[$i]."<br/>");
+}
+?>	
 </section>
 </div>
